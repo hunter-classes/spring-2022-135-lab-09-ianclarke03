610a006e25120e3c720de79cdcc5490037ff688a
ianclarke03 <83726782+ianclarke03@users.noreply.github.com>
2022-03-31 00:01:33 +0000

    c

--- funcs.cpp
+++ funcs.cpp
@@ -38,17 +38,27 @@ Coord3D * fartherFromOrigin(Coord3D *p1, Coord3D *p2){
 
 
 
+void move(Coord3D *ppos, Coord3D *pvel, double dt){
+  (*ppos).x = (*ppos).x + ((*pvel).x * dt);
+  (*ppos).y = (*ppos).y + ((*pvel).y * dt);
+  (*ppos).z = (*ppos).z + ((*pvel).z * dt);
+}
+
+
+
+
 
 
 int main() {
 
-  //Task A
+  std::cout << "Task A\n";
   Coord3D pointP = {10, 20, 30};
   std::cout << length(&pointP) << std::endl; // would print 37.4166
+  std::cout << "\n";
 
   
 
-  //Task B
+  std::cout << "Task B\n";
   Coord3D pointX = {10, 20, 30};
   Coord3D pointY = {-20, 21, -22};   
   
@@ -63,6 +73,23 @@ int main() {
     std::cout << "Point X is farther.\n";
   else if (ans == &pointY)
     std::cout << "Point Y is farther.\n";
+  std::cout << "\n";
+
+
+
+
+  std::cout << "Task C\n";
+  Coord3D pos = {0, 0, 100.0};
+  Coord3D vel = {1, -5, 0.2};
+
+  move(&pos, &vel, 2.0); // object pos gets changed
+  std::cout << pos.x << " " << pos.y << " " << pos.z << std::endl;
+  // prints: 2 -10 100.4
+  std::cout << "\n";
+
+
+
+  std::cout << "Task D\n";
 
 
   
